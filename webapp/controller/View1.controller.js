f88b502e68b07cee6eb87ceff011b0db6256ab21
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-01-27 01:51:35 +0530

    updated View and Controller

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,6 +4,19 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("GithubSAP-UI5-Fiori-Login-Page.controller.View1", {
+		
+		onBtnClick : function(){
+			var oUser = this.getView().byId("user").getValue();
+			var oPwd = this.getView().byId("pwd").getValue();
+			
+			if(oUser==="admin" && oPwd==="admin"){
+				document.write("Login Successfully");
+			}else{
+				alert("Re-Enter your Detail");
+			}
+			
+			
+		}
 
 	});
 });
